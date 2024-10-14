55ac12c924f4eb67d3845ae5d3a474d5e62d589d
Halleys123 <arnavadvik2003@gmail.com>
2024-10-15 03:15:25 +0530

    Initalization

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  body.addEventListener("click", function () {
+    console.log("Click");
+  });
+});
