9b711197aeaffbeb92921fc8d219be1e1ca497cf
Halleys123 <arnavadvik2003@gmail.com>
2024-10-26 21:52:52 +0530

    Valid path finder

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
+const IP = process.env.IP || "localhost";
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, IP, () => {
+  console.log(`Server running on http://${IP}:${PORT}`);
 });
