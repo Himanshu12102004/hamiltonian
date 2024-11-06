01d82ed0a47b7f69c10fae91a21e4354db3b4e44
himanshu <himanshu12104004@gmail.com>
2024-11-07 00:13:32 +0530

    resolved conflicts

--- frontend/src/Graph/main.ts
+++ frontend/src/Graph/main.ts
@@ -105,7 +105,7 @@ function isAnimationCompleted() {
     ap.frontendArray.pop();
     ap.frontendArrayPtr--;
     ap.backendArrayPtr++;
-    const event = new CustomEvent("pointerPostion");
+    const event = new CustomEvent('pointerPostion');
     document.dispatchEvent(event);
     return true;
   } else if (
@@ -116,7 +116,7 @@ function isAnimationCompleted() {
       ap.backendArray[ap.backendArrayPtr][1]
     ].addState(NodeState.selected);
     ap.backendArrayPtr++;
-    const event = new CustomEvent("pointerPostion");
+    const event = new CustomEvent('pointerPostion');
     document.dispatchEvent(event);
     return true;
   }
@@ -130,7 +130,7 @@ function startAnimation() {
   ) {
     if (ap.backendArrayPtr == -1) {
       ++ap.backendArrayPtr;
-      const event = new CustomEvent("pointerPostion");
+      const event = new CustomEvent('pointerPostion');
       document.dispatchEvent(event);
       ++ap.frontendArrayPtr;
       ap.frontendArray.push(
@@ -204,7 +204,7 @@ function startAnimation() {
             else ap.frontendArray[i].removeState(NodeState.rejected);
           }
           GlobalVariables.animationParams.backendArrayPtr++;
-          const event = new CustomEvent("pointerPostion");
+          const event = new CustomEvent('pointerPostion');
           document.dispatchEvent(event);
           GlobalVariables.animationConnectionWidth = currentAnimationWidth;
         }, 1000);
